ab9cc32239dbeeb7d77d1620c9d008bf72d55e59
노경준 <developerjun0615@gmail.com>
2021-10-29 01:02:08 +0900

    영속성 전이(CASCADE)와 고아 객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,31 +18,48 @@ public class JpaMain {
         tx.begin(); // 트랜잭션 시작
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            team.setName("teamB");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("hello1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("hello2");
-            member2.setTeam(teamB);
-            em.persist(member2);
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
+            tx.commit();
+
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
+//
+//            Team teamB = new Team();
+//            team.setName("teamB");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("hello1");
+//            member1.setTeam(team);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setUsername("hello2");
+//            member2.setTeam(teamB);
+//            em.persist(member2);
+//
+//            em.flush();
+//            em.clear();
+
 //            Member m = em.find(Member.class, member1.getId()); // 프록시로 가져옴
 
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .getResultList(); // TEAM 이 EAGER이라면 멤버의 갯수만큼
+//            List<Member> members = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList(); // TEAM 이 EAGER이라면 멤버의 갯수만큼
 
 
 //            System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
@@ -51,7 +68,7 @@ public class JpaMain {
 //            m.getTeam().getName(); // 직접적으로 getName 을 요청했으므로 초기화 되면서 Team을 가져옴
 //            System.out.println("====================");
 
-            tx.commit();
+//            tx.commit();
 
 //            Member member2 = new Member();
 //            member1.setUsername("hello2");
